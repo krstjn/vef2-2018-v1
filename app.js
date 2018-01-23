650f895cba9a8c0db6068b4acaf94c89d9ae4e4a
KristjanP <kth130@hi.is>
2018-01-23 22:46:08 +0000

    reynt að byrja

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+const fs = require('fs')
+
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('index', { title: 'Forsíða' });
+});
 
 app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
