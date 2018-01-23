d9f24f3c3de95f3dd9ca976904394077b3918e09
KristjanP <kth130@hi.is>
2018-01-23 23:45:18 +0000

    náði að fá þetta til að lesa úr markdown

--- app.js
+++ app.js
@@ -2,17 +2,38 @@ const fs = require('fs')
 
 const express = require('express');
 const path = require('path');
+const MarkdownIt = require('markdown-it');
+const fm = require('front-matter');
 
 const app = express();
 
 const hostname = '127.0.0.1';
 const port = 3000;
+const encoding = 'utf8';
+function read(file) {
+  const data = fs.readFileSync(file)
+
+  return data.toString(encoding);
+}
+
+function write(content) {
+  const md = new MarkdownIt();
+  const front = fm(content);
+  console.log(front);
+  const result = md.render(front.body);
+  return result;
+}
+const input = 'articles/batman-ipsum.md';
+const data = read(input);
 
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Forsíða' });
+  const body = write(data);  
+  res.render('index', { title: 'Forsíða', body});
 });
 
 app.listen(port, hostname, () => {
